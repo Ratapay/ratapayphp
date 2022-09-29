5175816830c0381c9b37ce08ae5dde2d051a0c3c
afifdiy <aizzulf@live.com>
2022-09-29 13:49:18 +0700

    [mod] update invoice creation example

--- examples/createInvoice.php
+++ examples/createInvoice.php
@@ -56,10 +56,50 @@ $invoice = new \ratapay\ratapayphp\Invoice([
     'url_failed' => 'https://mysite.com/failed'
 ]);
 
-$invoice->addItem($item);
-$invoice->addItem($item2);
-$invoice->addBeneficiary($beneficiary);
-$invoice->addBeneficiary($beneficiary2);
+$invoice->addItems([
+    [
+        'id' => 'item1',
+        'qty' => 1,
+        'subtotal' => 100000,
+        'name' => 'Item 1',
+        'type' => 'Digital',
+        'category' => "cat",
+        'brand' => 'brd',
+        'refundable' => true,
+        'refund_threshold' => '7D'
+    ],
+    [
+        'id' => 'item2',
+        'qty' => 1,
+        'subtotal' => 150000,
+        'name' => 'Item 2',
+        'type' => 'Digital',
+        'category' => "cat",
+        'brand' => 'brd',
+        'refundable' => true,
+        'refund_threshold' => '8D'
+    ]
+]);
+$invoice->addBeneficiaries([
+    [
+        'email' => 'jv1@mail.com',
+        'name' => 'name',
+        'username' => 'username',
+        'share_amount' => 50000,
+        'rebill_share_amount' => null,
+        'share_item_id' => 'item1',
+        'tier' => 1
+    ],
+    [
+        'email' => 'jv2@mail.com',
+        'name' => 'name',
+        'username' => 'username',
+        'share_amount' => 50000,
+        'rebill_share_amount' => null,
+        'share_item_id' => 'item2',
+        'tier' => 1
+    ]
+]);
 
 $client = new \ratapay\ratapayphp\Client(101, 'abc', 'def', 'ghi');
 $invoice = $client->createTransaction($invoice);
